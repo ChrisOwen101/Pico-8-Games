a711b744ffc006d3e04e64fc1208f860cfc06a93
Christopher Owen <chris.owen@sigmalabs.co.uk>
2024-05-21 22:30:02 +0100

    Add full roads

--- town_builder/main.lua
+++ town_builder/main.lua
@@ -1,9 +1,97 @@
 poke(0x5F2D, 1)
 
+camera_x = 0
+camera_y = 0
+
+crossroad = { x = 0, y = 96 }
+vertical_road = { x = 16, y = 96 }
+horizontal_road = { x = 32, y = 96 }
+turn_left_up = { x = 48, y = 96 }
+turn_right_up = { x = 48, y = 112 }
+turn_left_down = { x = 64, y = 112 }
+turn_right_down = { x = 32, y = 112 }
+horiztonal_up = { x = 64, y = 96 }
+horiztonal_down = { x = 80, y = 96 }
+vertical_left = { x = 96, y = 96 }
+vertical_right = { x = 112, y = 96 }
+blank = { x = 0, y = 112 }
+
+tiles = { {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {} }
+
+function draw_tile(tile, x, y)
+    sspr(tile.x, tile.y, 16, 16, x, y, 8, 8)
+end
+
+function choose_road_tile(x, y)
+    x = flr(x / 8) + 1
+    y = flr(y / 8) + 1
+
+    if tiles[x - 1] and tiles[x - 1][y] == "ROAD" and tiles[x + 1] and tiles[x + 1][y] == "ROAD" and tiles[x] and tiles[x][y - 1] == "ROAD" and tiles[x] and tiles[x][y + 1] == "ROAD" then
+        return crossroad
+    end
+
+    if tiles[x - 1] and tiles[x - 1][y] == "ROAD" and tiles[x + 1] and tiles[x + 1][y] == "ROAD" and tiles[x] and tiles[x][y - 1] == "ROAD" then
+        return horiztonal_up
+    end
+
+    if tiles[x - 1] and tiles[x - 1][y] == "ROAD" and tiles[x + 1] and tiles[x + 1][y] == "ROAD" and tiles[x] and tiles[x][y + 1] == "ROAD" then
+        return horiztonal_down
+    end
+
+    if tiles[x] and tiles[x][y - 1] == "ROAD" and tiles[x] and tiles[x][y + 1] == "ROAD" and tiles[x + 1] and tiles[x + 1][y] == "ROAD" then
+        return vertical_right
+    end
+
+    if tiles[x] and tiles[x][y - 1] == "ROAD" and tiles[x] and tiles[x][y + 1] == "ROAD" and tiles[x - 1] and tiles[x - 1][y] == "ROAD" then
+        return vertical_left
+    end
+
+    if tiles[x - 1] and tiles[x - 1][y] == "ROAD" and tiles[x] and tiles[x][y - 1] == "ROAD" then
+        return turn_left_up
+    end
+
+    if tiles[x + 1] and tiles[x + 1][y] == "ROAD" and tiles[x] and tiles[x][y - 1] == "ROAD" then
+        return turn_right_up
+    end
+
+    if tiles[x - 1] and tiles[x - 1][y] == "ROAD" and tiles[x] and tiles[x][y + 1] == "ROAD" then
+        return turn_left_down
+    end
+
+    if tiles[x + 1] and tiles[x + 1][y] == "ROAD" and tiles[x] and tiles[x][y + 1] == "ROAD" then
+        return turn_right_down
+    end
+
+    if tiles[x - 1] and tiles[x - 1][y] == "ROAD" or tiles[x + 1] and tiles[x + 1][y] == "ROAD" then
+        return vertical_road
+    end
+
+    if tiles[x] and tiles[x][y - 1] == "ROAD" or tiles[x] and tiles[x][y + 1] == "ROAD" then
+        return horizontal_road
+    end
+
+    return blank
+end
+
+function draw_tile_grid()
+    for x = 0, 128, 8 do
+        for y = 0, 128, 8 do
+            tile_row = tiles[flr(x / 8) + 1]
+
+            if tile_row then
+                tile = tile_row[flr(y / 8) + 1]
+
+                if tile and tile == "ROAD" then
+                    draw_tile(choose_road_tile(x, y), x, y)
+                end
+            end
+        end
+    end
+end
+
 function is_mouse_clicked()
     clicked = stat(34) == 1
 
-    -- if the mouse is over the cookie and the primary button is pressed
     if clicked then
         mouse_down = true
     end
@@ -34,23 +122,30 @@ function _update()
     end
 
     key_pressed = stat(31)
+
+    if is_mouse_clicked() then
+        x = flr(stat(32) / 8) + 1
+        y = flr(stat(33) / 8) + 1
+
+        if not tiles[x][y] then
+            tiles[x][y] = "ROAD"
+        end
+    end
 end
 
 function draw_mouse()
-    x = stat(32)
-    y = stat(33)
-    if clicked_this_frame then
-        sspr(112, 17, 16, 32, x, y)
-    else
-        sspr(0, 17, 16, 32, x, y)
-    end
+    x = flr(stat(32) / 8) * 8
+    y = flr(stat(33) / 8) * 8
+    sspr(0, 112, 16, 16, x, y, 8, 8)
 end
 
 function _draw()
     cls()
 
     -- draw the background
-    rectfill(-500, -500, 500, 500, 1)
+    rectfill(-500, -500, 500, 500, 3)
+
+    draw_tile_grid()
 
     -- draw unfixed items
 
